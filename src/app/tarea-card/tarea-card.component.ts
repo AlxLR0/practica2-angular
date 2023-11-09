cf137fd2045e935925e5db2cc78e8d32d9dd66a0
Alejandro Lopez Rubio <alex.lopez.rubio2600@gmail.com>
2023-11-09 12:49:32 -0600

    proyecto 2
    al 100

---
+++ src/app/tarea-card/tarea-card.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Tarea } from "../mi-componente/models/tarea.model";
+import { TareaCardItem } from '../mi-componente/models/tarea-card-item.model';
+import { TareaStatus } from "src/app/enum/tarea-status.enum";
+
+@Component({
+    selector: 'app-tarea-card',
+    templateUrl: './tarea-card.component.html'
+})
+export class TareaCardComponent {
+    @Input()
+    tarea!: Tarea;
+    @Input()
+    indice: number = -1;
+
+    @Output("onStatusChange") emitter: EventEmitter<TareaCardItem>;
+    tareaStatus = { 
+        Completado: TareaStatus.Completado,
+        Retrasado: TareaStatus.Retrasado,
+        Pendiente: TareaStatus.Pendiente
+
+    };
+
+    constructor() {
+        this.emitter = new EventEmitter();
+    }
+
+
+    cambiarStatus(status:TareaStatus): void {
+        this.tarea.status = status;
+
+        this.emitter.emit({
+            indice: this.indice,
+            tarea: this.tarea
+        });
+    }
+}
---
+++ src/app/tareas-module/tareas-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TareasModuleRoutingModule } from './tareas-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TareasModuleRoutingModule
+  ]
+})
+export class TareasModuleModule { }
